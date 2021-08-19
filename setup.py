5a6d9cf9c2eb87e96468ff3726b50a7a0ebeb18f
Paul Diederichs <paul.diederichs@disneyresearch.com>
2021-08-19 14:17:01 +0200

    MAINT: Package project

---
+++ setup.py
@@ -0,0 +1,32 @@
+import codecs
+from pathlib import Path
+from setuptools import setup, find_packages
+
+from exptorch import __version__
+
+
+readme_filename = Path(__file__).absolute().parent / "README.md"
+with codecs.open(readme_filename, encoding="utf-8") as file:
+    long_description = file.read()
+
+
+setup(
+    name="exptorch",
+    version=__version__,
+    description="basic pytorch wrapper and experiment scheduler",
+    long_description=long_description,
+    maintainer="Paul Aurel Diederichs",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["torch", "numpy", "tensorboard"],
+    extras_require=dict(
+        dev=[
+            "black",
+            "codespell",
+            "flake8",
+            "pydocstyle",
+            "pylint",
+            "pytest",
+            "pytest-cov",
+        ],
+    ),
+)
