78d3d99db089e11c68d182ec211e62a7bac2a667
Paul Aurel Diederichs <paulaureldiederichs@gmail.com>
2021-08-23 14:04:58 +0200

    MAINT: Add docstring and refactor validate_type

--- exptorch/utils/validation.py
+++ exptorch/utils/validation.py
@@ -1,3 +1,28 @@
-def _validate_type(obj, *, required_type, obj_name):
+from typing import Union, Tuple, Type
+
+
+def validate_type(obj, *, required_type: Union[Type, Tuple], obj_name: str):
+    """Validate that the object's type corresponds to specified required type.
+
+    Parameters
+    ----------
+    obj: object
+        Validated object.
+    required_type: Union[Type, Tuple]
+        Type or tuple of types specifying the object's required type(s).
+    obj_name: str
+        String specifying the object's name.
+
+    Raises
+    ------
+        Raises TypeError if the object does not correspond to the required type.
+    """
+    required_type = required_type if isinstance(required_type, tuple) else (required_type,)
+
     if not isinstance(obj, required_type):
-        raise TypeError()
+        required_type_name = tuple(_type.__name__ for _type in required_type)
+        error_msg = (
+            f"Require {obj_name} to be of type: {required_type_name}."
+            f" Given object has type {type(obj).__name__}."
+        )
+        raise TypeError(error_msg)
