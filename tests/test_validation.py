4628bbcf8a4c7420abd75e1851285263e5cddba0
Paul Aurel Diederichs <paulaureldiederichs@gmail.com>
2021-08-23 14:29:35 +0200

    ADD: Add test for validate_type

---
+++ tests/test_validation.py
@@ -0,0 +1,33 @@
+import pytest
+
+from exptorch import Struct, Params
+from exptorch.utils.validation import validate_type
+
+
+@pytest.mark.parametrize(
+    "obj, required_type, obj_name, expected_error_msg",
+    [
+        [
+            5,
+            float,
+            "learning_rate",
+            (
+                f"Require learning_rate to be of type: ('float',)."
+                f" Given object has type int."
+            ),
+        ],
+        [
+            dict(a=5, b=8, c=10),
+            (Struct, Params),
+            "experiment_parameters",
+            (
+                f"Require experiment_parameters to be of type: ('Struct', 'Params')."
+                f" Given object has type dict."
+            ),
+        ],
+    ],
+)
+def test_validate_type(obj, required_type, obj_name, expected_error_msg):
+    with pytest.raises(TypeError) as error:
+        validate_type(obj, required_type=required_type, obj_name=obj_name)
+    assert error.value.args[0] == expected_error_msg
