b5b29710b416dd9a54ca72e7f3b39d6b106378e5
Paul Aurel Diederichs <paulaureldiederichs@gmail.com>
2021-09-09 17:35:06 -0700

    MAINT: Refactor validate_type

--- exptorch/utils/validation.py
+++ exptorch/utils/validation.py
@@ -15,16 +15,21 @@ def validate_type(obj, *, required_type: Union[Type, Tuple], obj_name: str):
 
     Raises
     ------
+        Raises TypeError if the provided required_type is not a type or a tuple of types.
         Raises TypeError if the object does not correspond to the required type.
     """
     required_type = (
         required_type if isinstance(required_type, tuple) else (required_type,)
     )
+    if not all(isinstance(_type, type) for _type in required_type):
+        raise TypeError(
+            f"{validate_type.__name__} requires that second argument, required_type, is a type"
+            " or a tuple of types."
+        )
 
     if not isinstance(obj, required_type):
         required_type_name = tuple(_type.__name__ for _type in required_type)
-        error_msg = (
-            f"Require {obj_name} to be of type: {required_type_name}."
+        raise TypeError(
+            f"Require {obj_name} to be of type: {', '.join(required_type_name)}."
             f" Given object has type {type(obj).__name__}."
         )
-        raise TypeError(error_msg)
