b32d145c6d8bce1301f0d0a07067fa003ab1eee7
Paul Aurel Diederichs <paulaureldiederichs@gmail.com>
2021-09-10 10:24:12 -0700

    ADD: Add to test validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -5,6 +5,21 @@ from exptorch.utils.validation import validate_type
 
 
 @pytest.mark.parametrize(
+    "invalid_required_type", [[dict, Struct], (dict(a=5), dict), (5, 4), dict(a=5)]
+)
+def test_validate_type_raises_on_invalid_arg(invalid_required_type):
+    with pytest.raises(TypeError) as type_error:
+        validate_type(
+            obj=dict(a=5), required_type=invalid_required_type, obj_name="obj_name"
+        )
+    expected_error_msg = (
+        "validate_type requires that second argument, required_type, is a type"
+        " or a tuple of types."
+    )
+    assert type_error.value.args[0] == expected_error_msg
+
+
+@pytest.mark.parametrize(
     "obj, required_type, obj_name, expected_error_msg",
     [
         [
@@ -27,7 +42,9 @@ from exptorch.utils.validation import validate_type
         ],
     ],
 )
-def test_validate_type(obj, required_type, obj_name, expected_error_msg):
+def test_validate_type_raises_on_type_mismatch(
+    obj, required_type, obj_name, expected_error_msg
+):
     with pytest.raises(TypeError) as error:
         validate_type(obj, required_type=required_type, obj_name=obj_name)
     assert error.value.args[0] == expected_error_msg
