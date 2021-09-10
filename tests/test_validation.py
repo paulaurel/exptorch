2b66eb2cb2d3e8284142ad6b461b3189df4270e0
Paul Aurel Diederchs <36854635+paulaurel@users.noreply.github.com>
2021-09-10 10:46:53 -0700

    MAINT: Refactor tests
    Make variable names consistent across all tests.
    Reformat strings.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -45,6 +45,6 @@ def test_validate_type_raises_on_invalid_arg(invalid_required_type):
 def test_validate_type_raises_on_type_mismatch(
     obj, required_type, obj_name, expected_error_msg
 ):
-    with pytest.raises(TypeError) as error:
+    with pytest.raises(TypeError) as type_error:
         validate_type(obj, required_type=required_type, obj_name=obj_name)
-    assert error.value.args[0] == expected_error_msg
+    assert type_error.value.args[0] == expected_error_msg
