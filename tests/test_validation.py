aee9535c081422f3c054a27d86f400bbd9f3f667
Paul Aurel Diederchs <36854635+paulaurel@users.noreply.github.com>
2021-09-10 10:34:21 -0700

    MAINT: Refactor validate type function
    Reformat error message on raise. 
    Ensure input argument required_type is a type object or a tuple of type objects.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -5,6 +5,21 @@ from exptorch.utils.validation import validate_type
 
 
 @pytest.mark.parametrize(
+    "invalid_required_type", [[dict, Struct], (dict(a=5), dict), (5, 4), dict(a=5)]
+)
+def test_validate_type_raises_on_invalid_arg(invalid_required_type):
+    with pytest.raises(TypeError) as type_error:
+        validate_type(
+            obj=dict(a=5), required_type=invalid_required_type, obj_name="obj_name"
+        )
+    expected_error_msg = (
+        "validate_type requires that second argument, required_type, is a type"
+        " or a tuple of types."
+    )
+    assert type_error.value.args[0] == expected_error_msg
+
+
+@pytest.mark.parametrize(
     "obj, required_type, obj_name, expected_error_msg",
     [
         [
@@ -12,7 +27,7 @@ from exptorch.utils.validation import validate_type
             float,
             "learning_rate",
             (
-                "Require learning_rate to be of type: ('float',)."
+                "Require learning_rate to be of type: float."
                 " Given object has type int."
             ),
         ],
@@ -21,13 +36,15 @@ from exptorch.utils.validation import validate_type
             (Struct, Params),
             "experiment_parameters",
             (
-                "Require experiment_parameters to be of type: ('Struct', 'Params')."
+                "Require experiment_parameters to be of type: Struct, Params."
                 " Given object has type dict."
             ),
         ],
     ],
 )
-def test_validate_type(obj, required_type, obj_name, expected_error_msg):
+def test_validate_type_raises_on_type_mismatch(
+    obj, required_type, obj_name, expected_error_msg
+):
     with pytest.raises(TypeError) as error:
         validate_type(obj, required_type=required_type, obj_name=obj_name)
     assert error.value.args[0] == expected_error_msg
