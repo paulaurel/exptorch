e3a4f852c2b4a7213c4cb6b10af41fe96eddfbdd
Paul Aurel Diederchs <36854635+paulaurel@users.noreply.github.com>
2021-09-11 15:23:52 -0700

    FEAT: Add value validation to utils  (#17)

--- exptorch/utils/validation.py
+++ exptorch/utils/validation.py
@@ -33,3 +33,29 @@ def validate_type(obj, *, required_type: Union[Type, Tuple], obj_name: str):
             f"Require {obj_name} to be of type: {', '.join(required_type_name)}."
             f" Given object has type {type(obj).__name__}."
         )
+
+
+def validate_value(value, *, allowed_value, value_name):
+    """Validate that given value corresponds to the allowed_value.
+
+    Notes
+    -----
+    A tuple, as in ``validate_value(x, allowed_value=(A, B, ...), value_name="x")``,
+    may be given as the target to check against. This is equivalent to
+    ``validate_value(x, allowed_value=A, value_name="x")
+    or validate_value(x, allowed_value=B, value_name="x") or ...``
+
+    Raises
+    ------
+    Raises ValueError if the value does not correspond to the allowed_value.
+    """
+    allowed_value = (
+        allowed_value if isinstance(allowed_value, tuple) else (allowed_value,)
+    )
+
+    if value not in allowed_value:
+        raise ValueError(
+            f"Require that {value_name} corresponds to"
+            f" one of the following values: {', '.join(map(str, allowed_value))}."
+            f" Instead {value_name} has the value: {value}."
+        )
